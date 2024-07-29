060b127ad379a9be4d28b1de4043e89c177713ee
barenya123 <153221836+barenya123@users.noreply.github.com>
2024-07-29 15:10:09 +0530

    Add files via upload

---
+++ lecture-13/accessmodifiers2/C.java
@@ -0,0 +1,12 @@
+public class C extends A{
+    public static void main(String[] args) {
+        A obj = new A();
+        C chilobj = new C();
+        // System.out.println(obj.x);//default is not accessible outside the package
+        System.out.println(chilobj.x);//NOT VISIBLE
+        // System.out.println(obj.y);//private not accessible outside the class
+        System.out.println(chilobj.y);//NOT VISIBLE
+        System.out.println(obj.z);//public is accessible outside the class
+        System.out.println(chilobj.w);//protected will be visible in different package in subclass, using object of subclass 
+    }
+}
